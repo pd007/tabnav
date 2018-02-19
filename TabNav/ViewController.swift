841ca3c903b5c7e938ac08da280f5bbb0a40245f
Parth Dhebar <pdhebar@me.com>
2018-02-19 13:51:36 -0800

    Initial Commit

---
+++ TabNav/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  TabNav
+//
+//  Created by Parth Dhebar on 1/29/18.
+//  Copyright © 2018 Parth Dhebar. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        let someButton = UIButton()
+        someButton.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
